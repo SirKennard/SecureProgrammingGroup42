4df235ee64dcccdc91a84fabe1db854728959747
soupisnotveryepic <104745985+soupisnotveryepic@users.noreply.github.com>
2024-10-02 22:18:41 +0930

    Adding names to __init__.py

--- server/utils/__init__.py
+++ server/utils/__init__.py
@@ -1,3 +1,6 @@
+# COMP SCI 3307: Group 42 Implementation
+# Members: a1850028 Kanwartej Singh, a1853790 Christian Mignone, a1851275 Seung Lee, a1849563 Matthew Fuhlbohm
+
 from .RSA import RSA
 from .init_keys import init_keys
 
